e8c88c95196e44ab465cc2c03f089d8664f8835a
Nate Nevois <nnnevwa630@gmail.com>
2024-09-29 17:30:30 -0500

    Add ASP.NET Core backend

---
+++ todoApi/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace todoApi
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public string? Day { get; set; } 
+        public bool Reminder { get; set; }
+    }
+}
---
+++ todoApi/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace todoApi
+{
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
